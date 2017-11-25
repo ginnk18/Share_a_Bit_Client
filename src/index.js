c7521aa10c098d7967b231a5d8b09ee56d3f648f
Ginny <ginnykloos@gmail.com>
2017-11-25 11:24:12 -0800

    Begin set up of redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore();
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
