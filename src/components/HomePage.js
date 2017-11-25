76c31dbdf12bf00e2b003a97052aa41444af196e
Ginny <ginnykloos@gmail.com>
2017-11-25 15:19:16 -0800

    Center description on home page.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -15,6 +15,7 @@ class HomePage extends Component {
 			header: {
 				width: '50%',
 				display: 'flex',
+				flexDirection: 'column',
 				justifyContent: 'center',
 				alignItems: 'center'
 			}
@@ -24,6 +25,12 @@ class HomePage extends Component {
 				<div style={styles.jumbotron} class="row">
 					<div style={styles.header}>
   						<h1><strong>Share a Bit</strong></h1>
+  						<p class="pl-5">Pinterest kickstarter etsy blue bottle butcher.
+  						 Flannel taiyaki lo-fi cliche. Snackwave sartorial 
+  						 beard kombucha iPhone narwhal. Celiac vape brooklyn
+  						  franzen vexillologist squid. Meggings mlkshk authentic 
+  						  bicycle rights. Meggings slow-carb retro kinfolk, 
+  						  pop-up semiotics pitchfork.</p>
 					</div>
   					<img style={styles.img} src="/images/earth-leaves.png" />
   				</div>
